7a99e2bf779acff32490f2f733d30ad4feed5c97
shinyanakamura <nshinya@ieee.org>
2022-02-16 09:14:43 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,5 @@
 module "s3-bucket" {
   source  = "app.terraform.io/shinyanakamura-training/s3-bucket/aws"
   version = "2.2.0"
-  bucket_prefix = "shinyanakamura"
-  # insert required variables here
+ # insert required variables here
 }
